de8c1417092c9cea313964a9590e3d710337bafb
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-17 09:57:28 -0400

    initial commit moved hello to own component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Hello from './components/Hello';
+import './App.global.css';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
