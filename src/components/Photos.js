dc92aa7b5457de2df0a403ef8bfa6564c739f101
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-17 11:48:31 -0400

    allow user to select own photo and rotate

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,19 +1,47 @@
 import React, { useState, useCallback } from 'react'
 import Cropper from 'react-easy-crop'
+import { getOrientation } from 'get-orientation/browser'
+import { readFile, getRotatedImage } from '../utils/images'
+
+const ORIENTATION_TO_ANGLE = {
+  '3': 180,
+  '6': 90,
+  '8': -90,
+}
 
 function Photos(){
+  const [imageSrc, setImageSrc] = useState(null)
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
+
+  const onFileChange = async (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+      let imageDataUrl = await readFile(file)
+
+      // apply rotation if needed
+      const orientation = await getOrientation(file)
+      const rotation = ORIENTATION_TO_ANGLE[orientation]
+      if (rotation) {
+        imageDataUrl = await getRotatedImage(imageDataUrl, rotation)
+      }
+
+      setImageSrc(imageDataUrl)
+    }
+  }
   return(
     <div>
-      <Cropper
+      {!imageSrc
+      ? <input type="file" accept="image/*" onChange={onFileChange} />
+      : <Cropper
       crop={crop}
       zoom={zoom}
       onCropChange={setCrop}
       onZoomChange={setZoom}
-      image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+      image={imageSrc}
       aspect={4/3}
       />
+      }
     </div>
   )
 }
